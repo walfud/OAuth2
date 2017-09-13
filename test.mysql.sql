4b1f8aeee6ba8bd8cf861c40d1fb1bd850b03e08
walfud <hi@walfud.com>
2017-09-13 21:29:05 +0800

    debug for production

--- test.mysql.sql
+++ test.mysql.sql
@@ -1,7 +1,7 @@
 -- Clear
-DROP DATABASE IF EXISTS OAuth2;
-CREATE DATABASE OAuth2;
-USE OAuth2;
+DROP DATABASE IF EXISTS TestOauth2;
+CREATE DATABASE TestOauth2;
+USE TestOauth2;
 
 -- Table
 CREATE TABLE Users(
@@ -61,4 +61,44 @@ INSERT INTO Apps values(
     1, 
     'oauth2',
     NULL
+);
+
+-- Test Data
+INSERT INTO Users values(
+    1,
+    'test', 
+    'e10adc3949ba59abbe56e057f20f883e'      -- 123456
+);
+
+INSERT INTO Apps values(
+    2, 
+    'contactsync',
+    'http://contactsync.walfud.com/cb'
+);
+
+INSERT INTO Codes values(
+    1,
+    1,                                      -- test
+    2,                                      -- contactsync
+
+    '2b4db34a-7f7d-47cc-b349-cffbb3c192a9'
+);
+
+INSERT INTO Tokens values(
+    1,
+    1,                                      -- test
+    1,                                      -- oauth2
+
+    '469bd0c7-5d7e-45fd-9e46-5b09612a5193',
+    'f1cbd734-854a-c36f-091c-7559665151aa',
+    '954bd0c6-a37e-90fd-1246-4252612a0987'
+);
+INSERT INTO Tokens values(
+    1,
+    1,                                      -- test
+    2,                                      -- contactsync
+
+    '826658b4-9d7f-4eef-b083-923ab745b3ff',
+    '48d61801-8f27-496f-b25d-955f9b4d4af9',
+    '7971b59f-4bcf-431b-815a-2cf1b668a242'
 );
